e827685d3869554be9fa03c5353b68dc3b3f3e29
Priya-chatte <chatte.priya@gmail.com>
2021-05-18 17:59:06 +0530

    all done

---
+++ Testng_Script2/src/Gitti_pckg/Test_Date.java
@@ -0,0 +1,11 @@
+package Gitti_pckg;
+
+public class Test_Date {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("my name is priya");
+
+	}
+
+}
---
+++ Testng_Script2/src/Gitti_pckg/Test_Date2.java
@@ -0,0 +1,11 @@
+package Gitti_pckg;
+
+public class Test_Date2 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("my name is ashu");
+
+	}
+
+}
